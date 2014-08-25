f91559becc45a328d6f09ba03b4fe4b53574a958
EmilyPlatzer <emilyplatzer@gmail.com>
2014-08-24 21:49:04 -0600

    Reordered validations to get view error messages ordered correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_secure_password
 
   validates_presence_of :username, :email
+  has_secure_password
   validates_presence_of :password_confirmation, on: :create
 
 end
