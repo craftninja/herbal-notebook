28876db9d6018934642ef2effc0e6a17e5d05a91
EmilyPlatzer <emilyplatzer@gmail.com>
2014-08-24 20:03:19 -0600

    Removed unnecessary validation from User model...
    it was causing duplicate error messages in the view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :username, :email
-  validates_presence_of :password, :password_confirmation, on: :create
+  validates_presence_of :password_confirmation, on: :create
 
 end
