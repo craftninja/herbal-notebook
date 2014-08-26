d5054bde16de8bcbd9ae4f53c8c865780c49c3db
EmilyPlatzer <emilyplatzer@gmail.com>
2014-08-26 07:32:47 -0600

    Constrained routes to ones needed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
-  resources :users
-  resources :sessions
-  resources :herbs
+  resources :users, :only => [:new, :create, :show]
+  resources :sessions, :only => [:destroy, :new, :create]
+  resources :herbs, :only => [:index, :show]
   post '/find_herb' => 'herbs#find_herb'
 
 end
