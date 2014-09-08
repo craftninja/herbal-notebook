a504c81ef1e51b8961ea5a02c198e317e2ce65c3
EmilyPlatzer <emilyplatzer@gmail.com>
2014-09-07 21:11:20 -0600

    Added image location to plant table, refactored to use that data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826042349) do
+ActiveRecord::Schema.define(version: 20140908025749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140826042349) do
     t.text     "contraindications"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "preparations", force: true do |t|
